2d24d6635e768212580c6fb271c7a360bc062396
davidlopezsotelo <davidaquarium@yahoo.es>
2021-08-22 21:08:15 +0200

    VENTANA INTELIJ

---
+++ src/PruevaGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class PruevaGUI extends JFrame{
+    private JButton BOTONButton;
+    private JPanel panel1;
+    private JTextField escribeAquiTextField;
+    private JLabel texto_fijo;
+
+    public PruevaGUI(){
+       super();
+       setContentPane(panel1);
+   }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
